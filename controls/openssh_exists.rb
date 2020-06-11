a44b109c318c1b00017621cce3c1089dfeef9d19
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-11 19:14:24 +0100

    Update tests according to latest pattern, i.e, using File.join, etc.
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssh_exists.rb
+++ controls/openssh_exists.rb
@@ -1,17 +1,24 @@
 title 'Tests to confirm openssh exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'openssh')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-openssh_relative_path = input('command_path', value: '/sbin/sshd')
-openssh_installation_directory = command("hab pkg path #{plan_ident}")
-openssh_full_path = openssh_installation_directory.stdout.strip + "#{ openssh_relative_path}"
- 
+
 control 'core-plans-openssh-exists' do
   impact 1.0
   title 'Ensure openssh exists'
   desc '
-  '
-   describe file(openssh_full_path) do
+  Verify openssh by ensuring bin/sshd exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: 'sbin/sshd')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  describe file(command_full_path) do
     it { should exist }
   end
 end

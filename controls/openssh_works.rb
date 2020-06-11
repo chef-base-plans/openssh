09baac4ad97c4e31c6f0727e028dfc547d102d5d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-11 19:14:24 +0100

    Update tests according to latest pattern, i.e, using File.join, etc.
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssh_works.rb
+++ controls/openssh_works.rb
@@ -1,47 +1,31 @@
 title 'Tests to confirm openssh works as expected'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'openssh')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-openssh_path = command("hab pkg path #{plan_ident}")
-openssh_pkg_ident = ((openssh_path.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
 
-control 'core-plans-openssh-works-001' do
+control 'core-plans-openssh-works' do
   impact 1.0
-  title 'sshd is the expected version'
+  title 'Ensure openssh works as expected'
   desc '
+  Verify openssh by ensuring 
+  (1) its installation directory exists and 
+  (2) that it returns the expected version.
   '
-
-  describe openssh_path do
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
   
-  describe command("DEBUG=true; hab pkg exec #{openssh_pkg_ident} ssh -V") do
-    its('exit_status') { should eq 0 }
+  command_relative_path = input('command_relative_path', value: 'bin/sshd')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  describe command("#{command_full_path} -V") do
+    its('exit_status') { should eq 1 }
     its('stdout') { should be_empty }
-    its('stderr') { should match /^OpenSSH_7.5p1/ }
+    its('stderr') { should_not be_empty }
+    its('stderr') { should match /^OpenSSH_#{plan_pkg_version}/ }
   end
 end
-
-control 'core-plans-openssh-works-002' do
-  impact 1.0
-  title 'openssh habitat service is "up"'
-  describe command("hab svc status") do
-    its('exit_status') { should eq 0 }
-    its('stdout') { should_not be_empty }
-    its('stdout') { should match /(?<package>#{openssh_pkg_ident})\s+(?<type>standalone)\s+(?<desired>up)\s+(?<state>up)/ }
-    its('stderr') { should be_empty }
-  end
-end
-
-control 'core-plans-openssh-works-003' do
-  impact 1.0
-  title 'sshd process is running in the background'
-  describe command("hab pkg exec core/busybox-static ps -efl") do
-    its('exit_status') { should eq 0 }
-    its('stdout') { should_not be_empty }
-    its('stdout') { should match /\/hab\/pkgs\/#{openssh_pkg_ident}\/sbin\/sshd -D -f \/hab\/svc\/openssh\/config\/sshd_config/ }
-    its('stderr') { should be_empty }
-  end
-end
-
